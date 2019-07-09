852724857755b3d4f9519de8cd28dadaa9b47ba8
Ben Mitchinson <115bwm@gmail.com>
2019-07-08 20:00:45 -0500

    Working tidal delete all using fork

--- platformScripts/tidal/deleteAllTidal.ts
+++ platformScripts/tidal/deleteAllTidal.ts
@@ -1,18 +1,26 @@
 import Tidal = require('tidal-api-wrapper');
+// Using bmitchinson fork https://github.com/bmitchinson/tidal-api/tree/delete-playlist
 import { TidalPass, TidalUser } from '../serviceKeys';
 
 const tidal = new Tidal();
 
-const whitelistNames: string[] = []; // ['soft indie'];
+const whitelistPlaylistNames: string[] = []; // ['soft indie'];
 
-(async () => {
-  const result = await tidal.login(TidalUser, TidalPass);
-  const playlists = await tidal.getPlaylists();
-  const toDelete = playlists.filter((playlist) => {
-    return !whitelistNames.includes(playlist.title);
-  });
-  toDelete.forEach(async (playlist) => {
-    console.log('Removing ' + playlist.title + ' from Tidal:');
-    await tidal.deletePlaylist(playlist.uuid);
-  });
-})();
+const deleteAll = async () => {
+  await tidal.login(TidalUser, TidalPass);
+  try {
+    const playlists = await tidal.getPlaylists();
+
+    const toDelete = playlists.filter((playlist) => {
+      return !whitelistPlaylistNames.includes(playlist.title);
+    });
+
+    toDelete.forEach(async (playlist) => {
+      console.log('Removing ' + playlist.title + ' from Tidal:');
+      await tidal.deletePlaylist(playlist.uuid);
+    });
+
+  } catch (e) {
+    console.log('Error with Tidal');
+  }
+};
