eece2690dba16e6567942124e1a79bcedff5f506
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2019-07-19 00:05:14 -0500

    Todos on FBMeta interface

--- platformScripts/spotify/spotifyTypes.ts
+++ platformScripts/spotify/spotifyTypes.ts
@@ -1,7 +1,8 @@
 export interface IPlaylistFBMeta {
   folder: string | undefined; // if folder is null, it's excluded from FB
   followers: number;
-  // TODO: image url?
+  // TODO: image url? string[]? how do sizes work what do I get.
+  // TODO: artists: string[]
   id: string;
   isOwner: boolean;
   name: string;
